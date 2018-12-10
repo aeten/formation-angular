77414f293b489c3930ea656f573c38518fb1696c
Thomas Pérennou <thomas.perennou@viveris.fr>
2018-12-10 17:04:38 +0100

    Step 3

--- Tutorial/mise-en-oeuvre/src/app/user/user.component.ts
+++ Tutorial/mise-en-oeuvre/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, Renderer } from '@angular/core';
 import { User } from './user';
 
 @Component({
@@ -6,8 +6,8 @@ import { User } from './user';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
-
+export class UserComponent implements OnChanges, OnInit, AfterViewInit, AfterViewChecked, OnDestroy {
+  
   user: User = {
     id: 1,
     username: 'test',
@@ -20,9 +20,26 @@ export class UserComponent implements OnInit {
   dateFormat = "MM/dd/yy";
   image = 'https://www.starwars-universe.com/images/actualites/collection/sideshow/sideyodalegendary/sideyodalegendary_e.jpg';
 
-  constructor() { }
+  @ViewChild('input') input;
+
+  constructor(private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+      console.log('---> AfterViewInit fires <---');
+      this.renderer.invokeElementMethod(this.input.nativeElement, 'focus');
+  }
+  ngOnChanges(changes: import("c:/Users/Angular/formation-angular/formation-angular/Tutorial/mise-en-oeuvre/node_modules/@angular/core/src/metadata/lifecycle_hooks").SimpleChanges): void {
+    throw new Error("Method not implemented.");
+  }
+  ngAfterViewChecked(): void {
+    throw new Error("Method not implemented.");
+  }
+  ngOnDestroy(): void {
+    throw new Error("Method not implemented.");
+  }
 
   ngOnInit() {
+    console.log('---> OnInit fires <---');
   }
 
 }
