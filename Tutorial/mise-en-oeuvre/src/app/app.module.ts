f7911f0e310586bb332ab2a04d21625222a26fb7
Thomas Pérennou <thomas.perennou@viveris.fr>
2018-12-11 14:34:35 +0100

    Add navigation bar

--- Tutorial/mise-en-oeuvre/src/app/app.module.ts
+++ Tutorial/mise-en-oeuvre/src/app/app.module.ts
@@ -2,23 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MzNavbarModule, MzInputModule, MzButtonModule, MzValidationModule, MzSpinnerModule } from 'ngx-materialize';
+
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         UserComponent,
         UserDetailComponent,
-        HomeComponent
+        HomeComponent,
+        NavbarComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MzNavbarModule,
+        MzInputModule,
+        MzButtonModule,
+        MzValidationModule,
+        MzSpinnerModule
+
     ],
     providers: [],
     bootstrap: [AppComponent]
