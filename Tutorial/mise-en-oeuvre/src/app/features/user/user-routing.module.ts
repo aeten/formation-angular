d331136eb897d3b68fd64d3d5a17474b70fa8378
Thomas Pérennou <thomas.perennou@viveris.fr>
2018-12-12 11:40:03 +0100

    Step 13: Feature Module (update user routing module)

--- Tutorial/mise-en-oeuvre/src/app/features/user/user-routing.module.ts
+++ Tutorial/mise-en-oeuvre/src/app/features/user/user-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserNewComponent } from './user-new/user-new.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+
+const userRoutes: Routes = [
+    { path: '', component: UserComponent },
+    { path: ':id', component: UserDetailComponent },
+    { path: 'add/new', component: UserNewComponent },
+    { path: 'edit/:id', component: UserEditComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(userRoutes)],
+    exports: [RouterModule]
 })
 export class UserRoutingModule { }
