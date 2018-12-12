af8e1c856aae5f838c6d8484517062aab55010f3
Thomas Pérennou <thomas.perennou@viveris.fr>
2018-12-12 11:35:32 +0100

    Step 13: Feature Module (update user module)

--- Tutorial/mise-en-oeuvre/src/app/features/user/user.module.ts
+++ Tutorial/mise-en-oeuvre/src/app/features/user/user.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from './../../shared/shared.module';
 
 import { UserRoutingModule } from './user-routing.module';
 
+import { UserComponent } from './user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserNewComponent } from './user-new/user-new.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+    imports: [
+        SharedModule,
+        UserRoutingModule
+    ],
+    declarations: [
+        UserComponent,
+        UserDetailComponent,
+        UserNewComponent,
+        UserEditComponent,
+        UserDeleteComponent
+    ]
 })
 export class UserModule { }
